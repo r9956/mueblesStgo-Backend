fdfc61c13cd727c5b8bde9a75a3890c7054f9bce
r9956 <calderontorresraul@gmail.com>
2024-10-17 15:11:32 -0300

    Jenkinsfile + cleaning

--- src/test/java/com/example/mueblesStgoBackend/services/ExtraHoursAuthorizationServiceTest.java
+++ src/test/java/com/example/mueblesStgoBackend/services/ExtraHoursAuthorizationServiceTest.java
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
